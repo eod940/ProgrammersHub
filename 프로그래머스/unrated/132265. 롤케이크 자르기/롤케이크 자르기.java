3a765bba71ed5d7ef59c1758baee674454458d65
eod940 <51254234+eod940@users.noreply.github.com>
2023-08-30 21:53:54 +0900

    [unrated] Title: 롤케이크 자르기, Time: 236.48 ms, Memory: 171 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        
+        Set<Integer> setL = new HashSet<>();        
+        Map<Integer, Integer> mapR = new HashMap<>();
+        
+        for (int i = 0; i < topping.length; i++) {
+            mapR.put(topping[i], mapR.getOrDefault(topping[i], 0) + 1);
+        }
+        
+        for (int i = 0; i < topping.length; i++) {
+            setL.add(topping[i]);
+            mapR.put(topping[i], mapR.get(topping[i]) - 1);
+            
+            if (mapR.get(topping[i]) == 0) {
+                mapR.remove(topping[i]);
+            }
+            
+            if (setL.size() == mapR.size()) {
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
