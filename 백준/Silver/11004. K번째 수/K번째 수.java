0d3d6f46c77dae1428f19b934afea7ac5ff23d59
eod940 <51254234+eod940@users.noreply.github.com>
2023-09-15 10:33:14 +0900

    [Silver V] Title: K번째 수, Time: 1972 ms, Memory: 375420 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int n, k;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		System.out.print(arr[k - 1]);
+	}
+}
